607a9ea5f82288698ceed674f7def62e6fdbc625
realmankwon <realman0214@gmail.com>
2024-04-24 11:15:46 +0900

    feat: endurance added

--- src/services/fork-version-resolver/constants.ts
+++ src/services/fork-version-resolver/constants.ts
@@ -2,10 +2,12 @@ export enum Chains {
   MAINNET = 1,
   GOERLI = 5,
   HOLESKY = 17000,
+  ENDURANCE_MAINNET = 648,
 }
 
 export const CAPELLA_FORK_VERSIONS = {
   [Chains.MAINNET]: '0x03000000',
   [Chains.GOERLI]: '0x03001020',
   [Chains.HOLESKY]: '0x04017000',
+  [Chains.ENDURANCE_MAINNET]: '0x50000001',
 }
